d8f13d39fdd70a67723e7c4163b84e002b39f4d5
jaumealonso21 <jaumealonso@hotmail.com>
2016-10-19 17:48:58 +0200

    Versio 1.0

---
+++ public_html/m6uf1pt14.js
@@ -0,0 +1,72 @@
+
+function num() {
+    var num = arguments[0];
+    var resposta = "";
+    
+    if ((num % 2) === 0) {
+        resposta = "El num és parell";
+    } else {
+        resposta = "El num és imparell";
+    }
+    
+    document.getElementById("desti").innerHTML = resposta;
+    
+    return true;
+}
+
+function textMaj() {
+    var text = arguments[0];
+    var resposta = "";
+    
+    if(text === text.toUpperCase()) {
+        resposta = "La cadena només conté majúscules";
+    } else if (text === text.toLowerCase()) {
+        resposta = "La cadena només conté minúscules";
+    } else {
+        resposta = "La cadena conté majúscules i minúscules";
+    }
+    
+    document.getElementById("desti").innerHTML = resposta;
+    
+    return true;
+}
+
+function capicua() {
+    var text1 = arguments[0];
+    var text2 = arguments[0];
+    var resposta = "";
+    var matriu = [];
+    var matriu2 = [];
+    var x;
+    
+    
+    matriu = text1.split("").reverse();
+    matriu2 = text2.split("");
+    
+    //matriu = matriu.reverse();
+    var punt = true;
+    
+    console.log(matriu);
+    
+    /*if(matriu === matriu.reverse()){
+        console.log(matriu);
+    }*/
+    
+    for(x in matriu) {
+        if(matriu[x] !== matriu2[x]){
+            punt = false;
+            console.log("jdfskjfkhdjk");
+            break;
+        }
+    }
+  
+    if(punt) {
+        resposta = "És palíndrom";
+    } else {
+        resposta = "No és palíndrom";
+    }
+    
+    document.getElementById("desti").innerHTML = resposta;
+    
+    return true;
+}
